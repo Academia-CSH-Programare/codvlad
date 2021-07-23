6ab03780b1447fbc6829f498da647d5045c943c1
movieboyo <70149688+movieboyo@users.noreply.github.com>
2021-07-23 16:50:11 +0300

    Add files via upload

---
+++ tema.java
@@ -0,0 +1,15 @@
+package com.company;
+
+class Chestie {
+    int atrb1, atrb2, atrb3, atrb4;
+}
+class Main {
+    public static void main(String[] args) {
+        Chestie chestieinit = new Chestie();
+        System.out.println(chestieinit.atrb1);
+        System.out.println(chestieinit.atrb2);
+        System.out.println(chestieinit.atrb3);
+        System.out.println(chestieinit.atrb4);
+    }
+}
+
